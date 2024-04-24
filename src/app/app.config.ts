2a9b9a7863f7313977823f29ecca6f1c77720b69
LarsThoennes <larsthoennes@gmail.com>
2024-04-24 19:59:04 +0200

    Committing local changes before pulling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-180","appId":"1:1063885758156:web:55d61b46dbc48905ac6c69","storageBucket":"dabubble-180.appspot.com","apiKey":"AIzaSyD1QIosifbrMmf2Cis-tPblgDMk1JJmgGE","authDomain":"dabubble-180.firebaseapp.com","messagingSenderId":"1063885758156"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
