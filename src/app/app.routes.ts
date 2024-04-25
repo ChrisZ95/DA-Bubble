f3c635334935b00f31441bbdffdc5575109fa4c8
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-04-25 21:42:54 +0200

    styles in main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './main-content/main/main.component';
 import { LogInComponent } from './start-page/log-in/log-in.component';
+import { MainComponent } from './main-content/main/main.component';
 
 export const routes: Routes = [
   {path: '' , component: LogInComponent},
-  // {path: '' , component: MainComponent}
+  {path: 'main' , component: MainComponent}
 ];
--- src/app/main-content/main/main.component.ts
+++ src/app/main-content/main/main.component.ts
@@ -4,22 +4,19 @@ import { RouterOutlet } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { WorkspaceComponent } from '../../workspace/workspace/workspace.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatMenuModule, MatButtonModule, RouterOutlet],
+  imports: [MatMenuModule, MatButtonModule, RouterOutlet, WorkspaceComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  constructor(public dialog: MatDialog) {}
 
-  constructor(public dialog: MatDialog) {
-
-  }
-
-  openCreateChannelDialog(){
+  openCreateChannelDialog() {
     this.dialog.open(DialogCreateChannelComponent);
   }
-
 }
