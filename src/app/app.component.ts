6899a2f14e3c30618041a753fa8f160ea19661ba
LarsThoennes <larsthoennes@gmail.com>
2024-04-25 13:34:21 +0200

    nothing to commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DialogCreateChannelComponent } from './dialog-create-channel/dialog-create-channel.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatMenuModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dabubble';
+  title = 'DABubble';
 
   constructor(public dialog: MatDialog) {
-    
+
   }
 
   openCreateChannelDialog(){
