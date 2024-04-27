f1f491a1488fa8b1f8d8507f9b4f6b0a0bca12e7
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-04-27 10:22:57 +0200

    workspace style and functionality

--- src/app/main-content/main/main.component.ts
+++ src/app/main-content/main/main.component.ts
@@ -6,16 +6,61 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { WorkspaceComponent } from '../../workspace/workspace/workspace.component';
 import { DialogProfileComponent } from '../../dialog-profile/dialog-profile.component';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatMenuModule, MatButtonModule, RouterOutlet, WorkspaceComponent],
+  imports: [
+    MatMenuModule,
+    MatButtonModule,
+    RouterOutlet,
+    WorkspaceComponent,
+    CommonModule,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
+  animations: [
+    trigger('displayWorkspaceTrigger', [
+      state(
+        'true',
+        style({
+          width: '30%',
+          opacity: 1,
+        })
+      ),
+      state(
+        'false',
+        style({
+          width: '0%',
+          opacity: 0,
+        })
+      ),
+      transition('false <=> true', animate('1s linear')),
+    ]),
+    trigger('isThreadOpenTrigger', [
+      state(
+        'true',
+        style({
+          width: '40%',
+          opacity: 1,
+        })
+      ),
+      state(
+        'false',
+        style({
+          width: '0%',
+          opacity: 0,
+        })
+      ),
+      transition('false <=> true', animate('1s linear')),
+    ]),
+  ],
 })
 export class MainComponent {
   constructor(public dialog: MatDialog) {}
+  displayWorkspace: boolean = true;
 
   openCreateChannelDialog() {
     this.dialog.open(DialogCreateChannelComponent);
@@ -23,5 +68,8 @@ export class MainComponent {
 
   openProfileDialog() {
     this.dialog.open(DialogProfileComponent);
+  }
+  showHideWorkspace() {
+    this.displayWorkspace = !this.displayWorkspace;
   }
 }
