045c1bf88719e934c9ff6b370b60383c45624849
ChrisZ95 <160311410+ChrisZ95@users.noreply.github.com>
2024-04-30 12:12:52 +0200

    Update Channel-Info Dialog

--- src/app/dialog-channel-info/dialog-channel-info.component.ts
+++ src/app/dialog-channel-info/dialog-channel-info.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-channel-info',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './dialog-channel-info.component.html',
   styleUrl: './dialog-channel-info.component.scss'
 })
@@ -14,4 +16,31 @@ export class DialogChannelInfoComponent {
   closeChannelInfoDialog(): void {
     this.dialogRef.close();
   }
+
+  channelName = "# Entwicklerteam";
+    description = "Dieser Channel ist für alles rund um #dfsdf vorgesehen. Hier kannst du zusammen mit deinem Team Meetings abhalten, Dokumente teilen und Entscheidungen treffen.";
+    editedChannelName: string = '';
+    editedDescription: string = '';
+    editingName: boolean = false;
+    editingDescription: boolean = false;
+    isEditing: boolean = false;
+
+    toggleEditing(field: string) {
+        if (field === 'name') {
+            this.editingName = !this.editingName;
+        } else if (field === 'description') {
+            this.editingDescription = !this.editingDescription;
+        }
+        this.isEditing = this.editingName || this.editingDescription;
+    }
+
+    saveChanges() {
+        if (this.editingName) {
+            this.channelName = this.editedChannelName;
+        }
+        if (this.editingDescription) {
+            this.description = this.editedDescription;
+        }
+        this.toggleEditing('');
+    }
 }
