ad66a62234e0152d83b02ce04deb9ba932e19c95
ChrisZ95 <160311410+ChrisZ95@users.noreply.github.com>
2024-05-02 14:06:21 +0200

    Update Dialog Windows

--- src/app/dialog-add-people-to-new-channel/dialog-add-people-to-new-channel.component.ts
+++ src/app/dialog-add-people-to-new-channel/dialog-add-people-to-new-channel.component.ts
@@ -1,17 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-people-to-new-channel',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './dialog-add-people-to-new-channel.component.html',
   styleUrl: './dialog-add-people-to-new-channel.component.scss'
 })
 export class DialogAddPeopleToNewChannelComponent {
+  selectedOption: string = '';
+  buttonColor: string = '#686868';
+  personName: string = '';
+
   constructor(private dialogRef: MatDialogRef<DialogAddPeopleToNewChannelComponent>){}
 
   closeAddPeopleToNewChannelDialog(): void {
     this.dialogRef.close();
+  }
+
+  toggleButtonColor(): void {
+    this.buttonColor = !this.selectedOption ? '#686868' : '#444DF2';
+  }
+
+  createChannel(): void {
+  // Logik zum Erstellen des Kanals hier
   }
 }
