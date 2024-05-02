73177270769151d6974529fcf536671c0aa9c153
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-05-02 20:38:49 +0200

    load chats

--- src/app/chats/channelchat/channelchat.component.ts
+++ src/app/chats/channelchat/channelchat.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DialogMembersComponent } from '../../dialog-members/dialog-members.component';
 import { DialogChannelInfoComponent } from '../../dialog-channel-info/dialog-channel-info.component';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPeopleComponent } from '../../dialog-add-people/dialog-add-people.component';
 import { DialogContactInfoComponent } from '../../dialog-contact-info/dialog-contact-info.component';
 import { TextEditorComponent } from '../../shared/text-editor/text-editor.component';
+import { ChatService } from '../../services/chat.service';
+import { log } from 'console';
+import { doc, collection, getDocs } from 'firebase/firestore';
 
 @Component({
   selector: 'app-channelchat',
@@ -13,8 +16,9 @@ import { TextEditorComponent } from '../../shared/text-editor/text-editor.compon
   templateUrl: './channelchat.component.html',
   styleUrls: ['./channelchat.component.scss', '../chats.component.scss'],
 })
-export class ChannelchatComponent {
-  constructor(public dialog: MatDialog) {}
+export class ChannelchatComponent implements OnInit {
+  constructor(public dialog: MatDialog, public chats: ChatService) {}
+  messages: any;
 
   openMemberDialog() {
     this.dialog.open(DialogMembersComponent);
@@ -30,5 +34,17 @@ export class ChannelchatComponent {
 
   openContactInfoDialog() {
     this.dialog.open(DialogContactInfoComponent);
+  }
+  async loadMessages() {
+    const chatsRef = collection(this.chats.db, 'chats');
+    const querySnapshot = await getDocs(chatsRef);
+    querySnapshot.forEach((doc) => {
+      console.log('Nachricht ID:', doc.id);
+      console.log('Nachricht Daten:', doc.data());
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadMessages();
   }
 }
