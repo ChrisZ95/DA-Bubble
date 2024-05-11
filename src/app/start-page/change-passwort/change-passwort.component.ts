9c96293dd539331bac6d0340fd3aea583c349b7c
LarsThoennes <larsthoennes@gmail.com>
2024-05-11 12:34:10 +0200

    delete comment

--- src/app/start-page/change-passwort/change-passwort.component.ts
+++ src/app/start-page/change-passwort/change-passwort.component.ts
@@ -27,27 +27,13 @@ export class ChangePasswortComponent implements OnInit {
 
   ngOnInit() {
     debugger
-    // this.firestoreService.resetPasswordUserId$.subscribe(uid => {
-    //   this.userId = uid;
-    //   console.log('UserID für Passwortänderung:', uid);
-    // });
-
-    // Die aktuelle URL abrufen
-const currentUrl = window.location.href;
-
-// Die continueUrl extrahieren
-const continueUrlStart = currentUrl.indexOf('continueUrl=') + 'continueUrl='.length;
-const continueUrlEnd = currentUrl.indexOf('&', continueUrlStart);
-const continueUrl = currentUrl.substring(continueUrlStart, continueUrlEnd !== -1 ? continueUrlEnd : undefined);
-
-// Die URL dekodieren, um Sonderzeichen zu entschlüsseln
-const decodedContinueUrl = decodeURIComponent(continueUrl);
-
-// Die Benutzer-ID extrahieren
-this.userId = new URL(decodedContinueUrl).searchParams.get('userId');
-
-console.log(this.userId); // Ausgabe: Die userID aus der URL lautet 3hh8IcgMwqMFinyu8VYAKP2uJ7J2
-
+    const currentUrl = window.location.href;
+    const continueUrlStart = currentUrl.indexOf('continueUrl=') + 'continueUrl='.length;
+    const continueUrlEnd = currentUrl.indexOf('&', continueUrlStart);
+    const continueUrl = currentUrl.substring(continueUrlStart, continueUrlEnd !== -1 ? continueUrlEnd : undefined);
+    const decodedContinueUrl = decodeURIComponent(continueUrl);
+    this.userId = new URL(decodedContinueUrl).searchParams.get('userId');
+    console.log(this.userId);
   }
 
 
