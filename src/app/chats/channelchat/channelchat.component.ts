84e339151dd525e721caf323ee9e23342551aa5a
ChrisZ95 <160311410+ChrisZ95@users.noreply.github.com>
2024-05-31 19:17:09 +0200

    update

--- src/app/chats/channelchat/channelchat.component.ts
+++ src/app/chats/channelchat/channelchat.component.ts
@@ -1,13 +1,4 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  SimpleChanges,
-  AfterViewInit,
-  ElementRef,
-  ViewChild,
-  Output,
-} from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
 import { DialogMembersComponent } from '../../dialog-members/dialog-members.component';
 import { DialogChannelInfoComponent } from '../../dialog-channel-info/dialog-channel-info.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -16,15 +7,13 @@ import { DialogContactInfoComponent } from '../../dialog-contact-info/dialog-con
 import { TextEditorComponent } from '../../shared/text-editor/text-editor.component';
 import { ChatService } from '../../services/chat.service';
 import { debug, log } from 'console';
-import { doc, collection, getDocs } from 'firebase/firestore';
+import { collection } from 'firebase/firestore';
 import { CommonModule, NgFor } from '@angular/common';
 import { TimestampPipe } from '../../shared/pipes/timestamp.pipe';
 import { Channel } from './../../../models/channel.class';
 import { ChannelService } from '../../services/channel.service';
-import { Firestore, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, onSnapshot, Unsubscribe } from '@angular/fire/firestore';
 import { FirestoreService } from '../../firestore.service';
-import { query, where, Query, DocumentData } from 'firebase/firestore';
-import { EventEmitter } from 'stream';
 import { Subscription } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import  { MatIconModule } from '@angular/material/icon';
@@ -38,7 +27,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './channelchat.component.html',
   styleUrls: ['./channelchat.component.scss', '../chats.component.scss'],
 })
-export class ChannelchatComponent implements OnInit, AfterViewInit {
+export class ChannelchatComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(
     public dialog: MatDialog,
     public channelService: ChannelService,
@@ -46,12 +35,13 @@ export class ChannelchatComponent implements OnInit, AfterViewInit {
     public firestoreService: FirestoreService,
     public chatService: ChatService
   ) {
-    onSnapshot(collection(this.firestore, 'channels'), (list) => {
+    this.channelSnapshotUnsubscribe = onSnapshot(collection(this.firestore, 'channels'), (list) => {
       this.allChannels = list.docs.map((doc) => doc.data());
     });
-    onSnapshot(collection(this.firestore, 'chats'), (list) => {
+    this.chatSnapshotUnsubscribe = onSnapshot(collection(this.firestore, 'chats'), (list) => {
       this.allChats = list.docs.map((doc) => doc.data());
     });
+  
     for (let i = 0; i < this.channelService.messages.length; i++) {
       this.isHoveredArray.push(false);
     }
@@ -82,6 +72,8 @@ export class ChannelchatComponent implements OnInit, AfterViewInit {
   currentMessageIndex: number | null = null;
   editingMessageIndex: number | null = null;
   editedMessageText: string = '';
+  private channelSnapshotUnsubscribe: Unsubscribe | undefined;
+  private chatSnapshotUnsubscribe: Unsubscribe | undefined;
 
   openMemberDialog() {
     this.dialog.open(DialogMembersComponent);
@@ -130,6 +122,19 @@ export class ChannelchatComponent implements OnInit, AfterViewInit {
       const authorName = await this.channelService.getAuthorName(message.uid);
       return { ...message, authorName };
     }));
+    this.isHoveredArray = new Array(this.channelService.messagesWithAuthors.length).fill(false);
+  }
+
+  ngOnDestroy() {
+    if (this.channelSnapshotUnsubscribe) {
+      this.channelSnapshotUnsubscribe();
+    }
+    if (this.chatSnapshotUnsubscribe) {
+      this.chatSnapshotUnsubscribe();
+    }
+    if (this.channelSubscription) {
+      this.channelSubscription.unsubscribe();
+    }
   }
 
   async onChannelChange(channelId: string) {
