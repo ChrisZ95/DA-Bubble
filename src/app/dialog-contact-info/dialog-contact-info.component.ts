ade84333b2398e2947faaec6b9c11929b9fb4faa
LarsThoennes <larsthoennes@gmail.com>
2024-06-02 23:07:12 +0200

    open edit profile from dialog-contact-info

--- src/app/dialog-contact-info/dialog-contact-info.component.ts
+++ src/app/dialog-contact-info/dialog-contact-info.component.ts
@@ -1,7 +1,9 @@
+import { FirestoreService } from './../firestore.service';
 import { CommonModule } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogEditProfileComponent } from '../dialog-edit-profile/dialog-edit-profile.component';
 
 @Component({
   selector: 'app-dialog-contact-info',
@@ -13,6 +15,8 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class DialogContactInfoComponent implements OnInit{
   constructor(
     private dialogRef: MatDialogRef<DialogContactInfoComponent>,
+    private firestore: FirestoreService,
+    public dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
     onlineStatus: Date = new Date();
@@ -20,11 +24,20 @@ export class DialogContactInfoComponent implements OnInit{
     lastOnlineMessage: string = '';
     onlineStatusClass: string = '';
     signUpDate: Date = new Date();
+    userIsVerified: any;
+    myAccount: boolean = false;
 
   ngOnInit(): void {
     console.log(this.data);
     this.convertOnlineStatus(this.data.logIndate, this.data.logOutDate);
     this.convertSignUpDate(this.data.signUpdate);
+    this.userIsVerified = this.firestore.auth.currentUser.emailVerified;
+    const localStorageUid = localStorage.getItem('uid')
+    if(this.data.uid === localStorageUid) {
+      this.myAccount = true;
+    } else {
+      this.myAccount = false;
+    }
   }
 
   convertSignUpDate(unixTimestamp: any) {
@@ -41,6 +54,11 @@ export class DialogContactInfoComponent implements OnInit{
 
   closeContactInfoDialog() {
     this.dialogRef.close();
+  }
+
+  openEditProfile() {
+    this.closeContactInfoDialog();
+    this.dialog.open(DialogEditProfileComponent);
   }
 
   convertOnlineStatus(unixTimestampLogIn: any, unixTimestampLogOut: any): void {
@@ -68,13 +86,13 @@ export class DialogContactInfoComponent implements OnInit{
       this.lastOnlineMessage = 'Zuletzt online gerade eben';
       this.onlineStatusClass = 'offline';
     } else if (diffInMinutes < 60) {
-      this.lastOnlineMessage = `War zuletzt online vor ${diffInMinutes} Minuten`;
+      this.lastOnlineMessage = `Zuletzt online vor ${diffInMinutes} Minuten`;
       this.onlineStatusClass = 'offline';
     } else if (diffInHours < 24) {
-      this.lastOnlineMessage = `War zuletzt online vor ${diffInHours} Stunden`;
+      this.lastOnlineMessage = `Zuletzt online vor ${diffInHours} Stunden`;
       this.onlineStatusClass = 'offline';
     } else {
-      this.lastOnlineMessage = `War zuletzt online vor ${diffInDays} Tagen`;
+      this.lastOnlineMessage = `Zuletzt online vor ${diffInDays} Tagen`;
       this.onlineStatusClass = 'offline';
     }
   }
