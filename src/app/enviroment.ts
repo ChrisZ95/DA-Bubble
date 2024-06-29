3a1be021d2f680809714939d08da8521b6af521b
ChrisZ95 <160311410+ChrisZ95@users.noreply.github.com>
2024-06-29 23:47:03 +0200

    first commit

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,22 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'dabubble-180',
-    appId: '1:1063885758156:web:55d61b46dbc48905ac6c69',
-    storageBucket: 'dabubble-180.appspot.com',
-    apiKey: 'AIzaSyD1QIosifbrMmf2Cis-tPblgDMk1JJmgGE',
-    authDomain: 'dabubble-180.firebaseapp.com',
-    messagingSenderId: '1063885758156',
-    databaseURL:'https://dabubble-180-default-rtdb.europe-west1.firebasedatabase.app',
-    // apiKey: 'AIzaSyAZuqach9UwtPiJAt3hrD8SczbPUPTLC7k',
-    // authDomain: 'da181-db3ce.firebaseapp.com',
-    // databaseURL:
-    //   'https://da181-db3ce-default-rtdb.europe-west1.firebasedatabase.app',
-
-    // projectId: 'da181-db3ce',
-    // storageBucket: 'da181-db3ce.appspot.com',
-    // messagingSenderId: '841958832466',
-    // appId: '1:841958832466:web:c55e8fcb64fb1425bde28b',
-    // measurementId: 'G-KT2C4RDV9B',
+    projectId: 'da-bubble-ab311',
+    appId: '1:402912861096:web:a65571bad7f35945b59b9d',
+    storageBucket: 'da-bubble-ab311.appspot.com',
+    apiKey: 'AIzaSyCQ122T9_3f4ipLWnPrjgAo9wAO9RMqKk4',
+    authDomain: 'da-bubble-ab311.firebaseapp.com',
+    messagingSenderId: '402912861096',
+    databaseURL:'https://da-bubble-ab311-default-rtdb.europe-west1.firebasedatabase.app',
   },
 };
